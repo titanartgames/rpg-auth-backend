a8c848ea228a04c623311e0886a91b4f507b6933
titanartgames <159055091+titanartgames@users.noreply.github.com>
2026-03-29 21:36:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const admin = require("firebase-admin");
+
+const app = express();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+app.get("/login", async (req, res) => {
+  const uid = "player_" + Math.floor(Math.random() * 999999);
+  const token = await admin.auth().createCustomToken(uid);
+  res.json({ token });
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
